35b7f630d1847ba21bd8fd4a92f274d3cc4f5519
gizzard <adamumohammed2477@gmail.com>
2021-06-17 10:59:04 +0100

    my context-app

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,24 @@
+import {createContext, useState} from 'react'
+
+export const BookContext = createContext()
+
+function BookContextProvider({children}){
+    const [book, setBook] = useState([
+        {name:"Gods are not to blame", author:"Okonko", id: 1},
+        {name:"Country mouse and Town mouse", author:"Serwaa", id: 2}
+    ])
+    const addNewBook=(newbook)=>{
+        setBook([newbook, ...book])
+    }
+    const deleteBook=(id)=>{
+        setBook(book.filter((b)=>b.id!==id))
+    }
+    return(
+        <BookContext.Provider value = {{book, addNewBook, deleteBook}}>
+            {children}
+        </BookContext.Provider>
+
+    )
+}
+
+export default BookContextProvider
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
+import BookContextProvider from "./contexts/BookContext"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BookContextProvider>
+      <App />
+    </BookContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
